6a845260c681c29ac932ad0b25130f85a350d878
Renato Mendes <mendeslrenato@gmail.com>
2023-01-11 20:00:25 -0300

    feat: add DeleteClient path to clients route

--- backend/src/main/routes/clients-route.ts
+++ backend/src/main/routes/clients-route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
-import { makeLoadClientsController, makeAddClientController, makeUpdateClientController } from '../factories/presentation'
+import { makeLoadClientsController, makeAddClientController, makeUpdateClientController, makeDeleteClientController } from '../factories/presentation'
 
 export default (router: Router): void => {
   router.get('/clients', adaptRoute(makeLoadClientsController()))
   router.post('/clients', adaptRoute(makeAddClientController()))
   router.put('/clients', adaptRoute(makeUpdateClientController()))
+  router.delete('/clients', adaptRoute(makeDeleteClientController()))
 }
