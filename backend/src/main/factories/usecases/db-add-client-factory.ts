b9a1f4507e90637c8978b512551e77eccc923735
Renato Mendes <mendeslrenato@gmail.com>
2023-01-11 17:41:24 -0300

    feat: add DbAddClient factory

---
+++ backend/src/main/factories/usecases/db-add-client-factory.ts
@@ -0,0 +1,7 @@
+import { DbAddClient } from '../../../data/usecases'
+import { ClientMongoRepository } from '../../../infra/db'
+
+export const makeDbAddAclient = (): DbAddClient => {
+  const clientMongoRepository = new ClientMongoRepository()
+  return new DbAddClient(clientMongoRepository)
+}
--- backend/src/main/factories/usecases/index.ts
+++ backend/src/main/factories/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from './db-authentication-factory'
 export * from './db-load-clients-factory'
 export * from './remote-load-users-factory'
+export * from './db-add-client-factory'
