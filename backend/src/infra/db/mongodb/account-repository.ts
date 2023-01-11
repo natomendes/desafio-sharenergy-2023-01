3369b488bd016fef570c1e9dfae20cccc158d4be
Renato Mendes <mendeslrenato@gmail.com>
2023-01-10 23:25:45 -0300

    chore: add mongo connect to server start

---
+++ backend/src/infra/db/mongodb/account-repository.ts
@@ -0,0 +1,8 @@
+// import { LoadAccountByUser } from '../../../data/protocols'
+// import { AccountModel } from '../../../domain/models'
+
+// export class AccountMongoRepository implements LoadAccountByUser {
+//   async loadByUser (username: string): Promise<AccountModel> {
+
+//   }
+// }
---
+++ backend/src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUri: 'mongodb://root:123456@localhost:27017/sharenergy-db?authSource=admin',
+  port: 5050
+}
