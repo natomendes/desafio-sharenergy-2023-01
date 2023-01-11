7dfd47c0665510bde7c6c854bf97b411348291d5
Renato Mendes <mendeslrenato@gmail.com>
2023-01-11 19:07:31 -0300

    feat: add UpdateClientController

---
+++ backend/src/presentation/controllers/update-client-controller.ts
@@ -0,0 +1,24 @@
+import { UpdateClient } from '../../domain/usecases'
+import { badRequest, noContent, serverError } from '../helpers'
+import { Controller, ControllerRequest, ControllerResponse, Validation } from '../protocols'
+
+export class UpdateClientController implements Controller {
+  constructor (
+    private readonly validation: Validation,
+    private readonly updateClient: UpdateClient
+  ) {}
+
+  async handle (request: ControllerRequest): Promise<ControllerResponse> {
+    try {
+      const error = this.validation.validate(request.body)
+      if (error) return badRequest(error)
+
+      const { name, email, phone, address, cpf } = request.body
+      await this.updateClient.update({ name, email, phone, address, cpf })
+
+      return noContent()
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
