9a4acdb1db8ac1e07e5516cf2e707f387f8aa87c
Renato Mendes <mendeslrenato@gmail.com>
2023-01-11 00:58:43 -0300

    feat: add login route composition

--- backend/src/infra/criptography/bcrypt-adapter.ts
+++ backend/src/infra/criptography/bcrypt-adapter.ts
@@ -3,6 +3,6 @@ import bcrypt from 'bcrypt'
 
 export class BcryptAdapter implements HashComparer {
   async compare (value: string, hashedValue: string): Promise<boolean> {
-    return bcrypt.compare(value, hashedValue)
+    return await bcrypt.compare(value, hashedValue)
   }
 }
--- backend/src/main/config/routes.ts
+++ backend/src/main/config/routes.ts
@@ -1,8 +1,10 @@
 import { Express, Router } from 'express'
 import loadUsersRoute from '../routes/load-users-route'
+import loginRoute from '../routes/login-route'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/', router)
   loadUsersRoute(router)
+  loginRoute(router)
 }
