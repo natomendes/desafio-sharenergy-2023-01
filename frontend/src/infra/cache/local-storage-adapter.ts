0b64fba06b8cb99d9e1991bbf2f06179ec969df5
Renato Mendes <mendeslrenato@gmail.com>
2023-01-12 18:06:11 -0300

    feat: add LocalStorageAdapter

---
+++ frontend/src/infra/cache/local-storage-adapter.ts
@@ -0,0 +1,7 @@
+import { SetStorage } from '@/data/protocols/cache'
+
+export class LocalStorageAdapter implements SetStorage {
+  set (key: string, value: any): void {
+    localStorage.setItem(key, JSON.stringify(value))
+  }
+}
