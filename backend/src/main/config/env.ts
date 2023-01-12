7da15b279ad44d3e41ed11111627036a06352864
Renato Mendes <mendeslrenato@gmail.com>
2023-01-11 23:17:07 -0300

    feat: add backend service to docker compose config

--- backend/src/main/config/env.ts
+++ backend/src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUri: 'mongodb://root:123456@localhost:27017/sharenergy-db?authSource=admin',
+  mongoUri: process.env.MONGO_URI || 'mongodb://root:123456@localhost:27017/sharenergy-db?authSource=admin',
   port: 5050,
   jwtSecret: 'sh4r3n3rgyS3cr3t'
 }
