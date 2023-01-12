48f955b08409f883168111185e15d61d1413bf09
Renato Mendes <mendeslrenato@gmail.com>
2023-01-11 22:41:07 -0300

    feat: add express middleware adapter

---
+++ backend/src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express'
+import { Controller, ControllerRequest } from '../../presentation/protocols'
+
+export const adaptMiddleware = (middleware: Controller) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: ControllerRequest = {
+      headers: req.headers
+    }
+    const httpResponse = await middleware.handle(httpRequest)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, httpResponse.body)
+
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
+    }
+  }
+}
