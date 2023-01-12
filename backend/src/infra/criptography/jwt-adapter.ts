f68589dc05ea32d2758233a420b147362b881ad4
Renato Mendes <mendeslrenato@gmail.com>
2023-01-11 22:24:37 -0300

    feat: add Decrypter to JwtAdapter

--- backend/src/infra/criptography/jwt-adapter.ts
+++ backend/src/infra/criptography/jwt-adapter.ts
@@ -1,10 +1,20 @@
-import { Encrypter } from '../../data/protocols'
+import { Decrypter, Encrypter } from '../../data/protocols'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (username: string, id: string): Promise<string> {
     return jwt.sign({ username, id }, this.secret)
+  }
+
+  async decrypt (accessToken: string): Promise<string> {
+    try {
+      const value = jwt.verify(accessToken, this.secret)
+
+      return value as any
+    } catch (error) {
+      return null
+    }
   }
 }
