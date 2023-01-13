eaf5761657ba80da77e3d6d99e72ef9aa21e1356
Renato Mendes <mendeslrenato@gmail.com>
2023-01-13 01:30:53 -0300

    feat: add SearchInput component

---
+++ frontend/src/presentation/components/SearchInput/index.tsx
@@ -0,0 +1,46 @@
+import { useRef } from 'react'
+
+export const SearchInput: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>()
+
+  return (
+    <div
+      className={`
+      text-slate-500
+      flex w-1/2
+      border border-slate-500 rounded-full py-1 px-1
+      focus-within:w-full focus-within:px-2
+      transition-all ease-in-out duration-500
+    `}>
+      <svg
+        width="24"
+        height="24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        aria-hidden="true"
+        className="flex-none peer"
+      >
+        <path d="m19 19-3.5-3.5"></path>
+        <circle cx="11" cy="11" r="6"></circle>
+      </svg>
+      <input
+        ref={inputRef}
+        type="text"
+        name="search"
+        placeholder="Busca Rapida"
+        autoComplete="off"
+        className={`
+          bg-transparent
+          text-slate-500
+          placeholder-slate-500
+          outline-none
+          text-center
+          w-full
+        `}
+      />
+    </div>
+  )
+}
