8e64943913109ec33d28b93a236ad8ba756cfbd8
Renato Mendes <mendeslrenato@gmail.com>
2023-01-13 16:33:50 -0300

    refactor: change LoadUsers method from get to post

--- backend/src/main/routes/users-route.ts
+++ backend/src/main/routes/users-route.ts
@@ -4,5 +4,5 @@ import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAuthMiddleware, makeLoadUsersController } from '../factories/presentation'
 
 export default (router: Router): void => {
-  router.get('/users', adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeLoadUsersController()))
+  router.post('/users', adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeLoadUsersController()))
 }
