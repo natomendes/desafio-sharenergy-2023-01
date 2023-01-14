8d0051d528d4d807bd4ab103d33303b3937e7f22
Renato Mendes <mendeslrenato@gmail.com>
2023-01-14 20:35:19 -0300

    feat: implement refresh Dog image function

--- frontend/src/main/factories/usecases/remote-get-random-dog-factory.ts
+++ frontend/src/main/factories/usecases/remote-get-random-dog-factory.ts
@@ -2,5 +2,7 @@ import { RemoteGetRandomDog } from '@/data/usecases/remote-get-random-dog'
 import { makeAxiosHttpClient } from '../http'
 
 export const makeRemoteGetRandomDog = (): RemoteGetRandomDog => {
-  return new RemoteGetRandomDog('https://random.dog/woof.json', makeAxiosHttpClient())
+  return new RemoteGetRandomDog(
+    'https://random.dog/woof.json?filter=mp4,webm',
+    makeAxiosHttpClient())
 }
--- frontend/src/presentation/pages/random-dog/index.tsx
+++ frontend/src/presentation/pages/random-dog/index.tsx
@@ -1,18 +1,56 @@
+import { GetRandomdog } from '@/domain/usecases'
 import { Header } from '@/presentation/components'
+import { useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
-export const RandomDog: React.FC = () => {
+type Props = {
+  getRandomDog: GetRandomdog
+}
+
+export const RandomDog: React.FC<Props> = ({ getRandomDog }: Props) => {
+  const imageUrl = useLoaderData() as string
+  const [imgSrc, setImgSrc] = useState(imageUrl)
+
+  const refreshImage = async (): Promise<void> => {
+    const url = await getRandomDog.get()
+    setImgSrc(url)
+  }
+
   return (
     <div className={`
       bg-gradient-to-tr from-primary to-green-600/60
-      min-h-screen flex flex-col      
+      h-screen flex flex-col 
     `}>
       <Header />
       <main className={`
-        p-2 flex flex-col items-center gap-2 flex-grow
+        p-2 flex flex-col items-center gap-6 flex-grow
         lg:p-3 lg:max-w-5xl
         md:max-w-3xl
         mx-auto w-full
       `}>
+        <button
+          onClick={async () => { await refreshImage() }}
+          className={`
+            bg-primary rounded-lg p-2.5 leading-5 text-white shadow
+            ring-white ring-opacity-60 ring-offset-2 ring-offset-primary
+            border-2 border-yellowOrange
+            active:outline-none active:ring-2 active:text-yellowOrange         
+          `}
+        >
+          Gerar nova imagem
+        </button>
+        <div className={`
+          max-w-md lg:max-w-xl
+          p-1 rounded-lg bg-yellowOrange
+        `}>
+          <img
+            src={imgSrc}
+            alt="Random dog image"
+            className={`
+              rounded-lg object-cover max-h-[600px]
+            `}
+          />
+        </div>
       </main>
     </div>
   )
