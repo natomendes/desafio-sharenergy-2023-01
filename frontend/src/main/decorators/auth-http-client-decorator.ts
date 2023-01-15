19f1dc3f05113388c0f4dbe939f0d314a1154d13
Renato Mendes <mendeslrenato@gmail.com>
2023-01-15 01:24:59 -0300

    feat: add AuthHttpClientDecorator

---
+++ frontend/src/main/decorators/auth-http-client-decorator.ts
@@ -0,0 +1,23 @@
+import { GetStorage } from '@/data/protocols/cache'
+import { HttpClient, HttpRequest, HttpResponse } from '@/data/protocols/http/http-client'
+import { AccountModel } from '@/domain/models'
+
+export class AuthHttpClientDecorator implements HttpClient {
+  constructor (
+    private readonly getStorage: GetStorage,
+    private readonly httpClient: HttpClient
+  ) {}
+
+  async request (data: HttpRequest): Promise<HttpResponse<any>> {
+    const account = this.getStorage.get('account') as AccountModel
+    if (account?.accessToken) {
+      data.headers = {
+        ...(data.headers || {}),
+        'x-access-token': account.accessToken
+      }
+    }
+
+    const httpResponse = await this.httpClient.request(data)
+    return httpResponse
+  }
+}
