8cc55c4cf1ffedc971103f66b3d13d09012b60f5
Renato Mendes <mendeslrenato@gmail.com>
2023-01-17 01:39:48 -0300

    refactor: change Submit button implementation to be more generic

--- frontend/src/presentation/components/SubmitButton/index.tsx
+++ frontend/src/presentation/components/SubmitButton/index.tsx
@@ -6,11 +6,11 @@ type Props = {
 }
 
 export const SubmitButton: React.FC<Props> = (props: Props) => {
-  const { state: { username, password } } = useContext(FormContext)
+  const { state } = useContext(FormContext)
   return (
     <button
       type="submit"
-      disabled={ !username || !password }
+      disabled={ !Object.values(state).every(el => el) }
       className={`
         bg-primary text-white font-bold leading-8
         rounded-md mt-4 w-full
