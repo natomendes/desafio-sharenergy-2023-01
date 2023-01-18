410b8ea15b466352373a1f4fb84bb3014fb3c2a4
Renato Mendes <mendeslrenato@gmail.com>
2023-01-18 11:19:35 -0300

    refactor: organize User page markup

--- frontend/src/presentation/pages/user/index.tsx
+++ frontend/src/presentation/pages/user/index.tsx
@@ -1,8 +1,8 @@
-import { UserModel } from '@/domain/models'
-import { LoadUsers } from '@/domain/usecases/load-users'
 import { ChangePage, Header, SearchInput, UserItem } from '@/presentation/components'
-import { useState } from 'react'
+import { LoadUsers } from '@/domain/usecases'
+import { UserModel } from '@/domain/models'
 import { useLoaderData } from 'react-router-dom'
+import { useState } from 'react'
 
 type Props = {
   loadUsers: LoadUsers
@@ -10,47 +10,35 @@ type Props = {
 
 export const User: React.FC<Props> = ({ loadUsers }: Props) => {
   const data = useLoaderData() as UserModel[] | null
+  const [pageData, setPageData] = useState({ page: 1, users: data })
   const [searchParam, setSearchParam] = useState('')
-  const [pageData, setPageData] = useState({
-    page: 1,
-    users: data
-  })
 
   const { page, users } = pageData
 
   const filterUsers = (users: UserModel[]): UserModel[] => {
-    if (!searchParam) return users
-
-    return users.filter(({ name }: UserModel) => name.toLowerCase().startsWith(searchParam.toLowerCase()))
+    return searchParam
+      ? users.filter(({ name }: UserModel) => name.toLowerCase().startsWith(searchParam.toLowerCase()))
+      : users
   }
 
   const getPage = async (event: React.MouseEvent<HTMLButtonElement>): Promise<void> => {
-    const buttonEl = event.currentTarget as HTMLButtonElement
-    const pageToGo = buttonEl.name === 'prev' ? page - 1 : page + 1
+    const pageToGo = event.currentTarget.name === 'prev' ? page - 1 : page + 1
     if (pageToGo > 0) {
       const newUsers = await loadUsers.load(`${pageToGo}`)
-      setPageData({
-        page: pageToGo,
-        users: newUsers
-      })
+      setPageData({ page: pageToGo, users: newUsers })
       setSearchParam('')
     }
   }
 
   return (
-    <div className={`
-      bg-gradient-to-tr from-primary to-green-600/60
-      min-h-screen flex flex-col
-    `}>
+    <div className='bg-gradient-to-tr from-primary to-green-600/60 min-h-screen flex flex-col'>
       <Header />
       <main className={`
         p-2 flex flex-col items-center gap-2 flex-grow
         lg:p-3 lg:max-w-5xl
         mx-auto w-full
       `}>
-        <div className={`
-          flex justify-between gap-1 w-full
-        `}>
+        <div className='flex justify-between gap-1 w-full'>
           <ChangePage name="prev" prev={true} disabled={page === 1} onClick={getPage} />
           <SearchInput searchParam={searchParam} setSearchParam={setSearchParam } />
           <ChangePage name="next" prev={false} onClick={getPage} />
