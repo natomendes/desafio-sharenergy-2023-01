251fb561a3559fafb467c1414047792497f9500a
Renato Mendes <mendeslrenato@gmail.com>
2023-01-18 19:41:40 -0300

    refactor: remove Menu from Login page

--- frontend/src/presentation/components/Header/index.tsx
+++ frontend/src/presentation/components/Header/index.tsx
@@ -1,15 +1,22 @@
 import { DropdownMenu } from '@/presentation/components'
 import logo from '@/assets/images/logo_color.png'
+import { useLocation } from 'react-router-dom'
+import { classNames } from '@/presentation/utils'
 
 export const Header: React.FC = () => {
+  const path = useLocation().pathname
   return (
     <header className={`
     bg-gradient-to-tr from-gray-200 to-white p-3 shadow
     flex justify-end max-h-12
     border-t-4 border-primary
   `}>
-      <div className="max-w-5xl mx-auto w-full flex justify-between items-center relative">
-        <DropdownMenu />
+      <div className={classNames(
+        'max-w-5xl mx-auto w-full flex items-center relative',
+        path === '/login' ? 'justify-end' : 'justify-between'
+      )}
+      >
+        { path !== '/login' && <DropdownMenu />}
         <img
           src={logo}
           alt="Sharenergy logo"
