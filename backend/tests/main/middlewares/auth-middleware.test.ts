885acb008d884b2bcb0bffbe0b244aff61f67c93
Renato Mendes <mendeslrenato@gmail.com>
2023-01-19 03:16:08 -0300

    test: ensure AuthMiddleware return 403 if no account is found with token provided

--- backend/tests/main/middlewares/auth-middleware.test.ts
+++ backend/tests/main/middlewares/auth-middleware.test.ts
@@ -1,7 +1,16 @@
 import request from 'supertest'
 import app from '../../../src/main/config/app'
+import jwt from 'jsonwebtoken'
+import { MongoHelper } from '../../../src/infra/db'
 
 describe('User Route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
   it('Should return 403 if no token is provided', async () => {
     await request(app)
       .post('/users')
@@ -9,6 +18,14 @@ describe('User Route', () => {
   })
 
   it('Should return 403 if token is invalid', async () => {
+    await request(app)
+      .post('/users')
+      .set('x-access-token', 'invalid_token')
+      .expect(403)
+  })
+
+  it('Should return 403 if no account is found with token provided', async () => {
+    jest.spyOn(jwt, 'verify').mockImplementationOnce(() => 'invalid_token')
     await request(app)
       .post('/users')
       .set('x-access-token', 'invalid_token')
