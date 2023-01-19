bdb9ad0860cc8b803793b38d960233f9a214c9d1
Renato Mendes <mendeslrenato@gmail.com>
2023-01-19 01:10:19 -0300

    chore: add test:watch and test:ci scripts

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/tests'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
+    '<rootDir>/src/**/*.ts'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
