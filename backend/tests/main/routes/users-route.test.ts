f03a42320e5829bb7c303963b22c888ec513c71a
Renato Mendes <mendeslrenato@gmail.com>
2023-01-19 01:47:20 -0300

    test: ensure Users route returns 400 if no page is provided

---
+++ backend/tests/main/routes/users-route.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import { DbLoadAccountByToken } from '../../../src/data/usecases/db-load-account-by-token'
+import app from '../../../src/main/config/app'
+
+describe('Login Routes', () => {
+  beforeEach(() => {
+    jest.spyOn(DbLoadAccountByToken.prototype, 'load')
+      .mockResolvedValueOnce({
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        username: 'any_username',
+        password: 'any_password'
+      })
+  })
+  it('Should return 400 if no page is provided', async () => {
+    await request(app)
+      .post('/users')
+      .set('x-access-token', 'any_token')
+      .expect(400)
+  })
+})
