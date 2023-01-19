94500afaef3b65409791c728a5d08292e021eede
Renato Mendes <mendeslrenato@gmail.com>
2023-01-19 00:58:01 -0300

    chore: add jest and jest-mongo

---
+++ backend/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
