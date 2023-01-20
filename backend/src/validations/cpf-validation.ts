4a5b2ae150b8b3b830c40d0b483c239fbda3157b
Renato Mendes <mendeslrenato@gmail.com>
2023-01-19 22:20:17 -0300

    feat: add CpfValidation

---
+++ backend/src/validations/cpf-validation.ts
@@ -0,0 +1,44 @@
+import { InvalidFieldError } from '../presentation/errors'
+import { Validation } from '../presentation/protocols'
+
+export class CpfValidation implements Validation {
+  private readonly multipliers = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+  constructor (readonly field: string) {}
+
+  validate (input: object): Error {
+    const isValid = this.isValid(input[this.field])
+
+    return (!input[this.field] || isValid) ? null : new InvalidFieldError(input[this.field])
+  }
+
+  private isValid (cpf: string): boolean {
+    if (cpf.length !== 11) return false
+    if (cpf === '00000000000') return false
+    if (cpf.split('').some((digit) => isNaN(Number(digit)))) return false
+
+    const digitsArray = cpf.split('').slice(0, 9)
+    const verifiersDigits = cpf.slice(9)
+    if (!this.validation(digitsArray, verifiersDigits[0])) return false
+
+    this.multipliers.unshift(11)
+    digitsArray.push(verifiersDigits[0])
+    if (!this.validation(digitsArray, verifiersDigits[1])) {
+      this.multipliers.shift()
+      return false
+    }
+
+    this.multipliers.shift()
+    return true
+  }
+
+  private validation (digits: string[], verifier: string): boolean {
+    const digitsSum = digits.reduce((product, current, index) => {
+      return product + (Number(current) * this.multipliers[index])
+    }, 0)
+
+    const rest = (digitsSum * 10) % 11 === 10 || (digitsSum * 10) % 11 === 11 ? 0 : (digitsSum * 10) % 11
+    if (rest !== Number(verifier)) return false
+
+    return true
+  }
+}
